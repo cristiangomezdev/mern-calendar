b19a8b83bc23dcff1c650a92bb67e5443a8ed990
cristian <cristiangomez.dev@gmail.com>
2022-03-08 05:53:15 -0300

    paste build in project

---
+++ public/precache-manifest.c48dd8216063556fe36809a8ffcaa472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87d43291474732210c09e99b607dd5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984411130a0e85506430",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "a21828de0dd19d8858f9",
+    "url": "/static/css/main.00b6d57c.chunk.css"
+  },
+  {
+    "revision": "984411130a0e85506430",
+    "url": "/static/js/2.e179491d.chunk.js"
+  },
+  {
+    "revision": "ba0bd5f7ad564a5e3cfd56050486d288",
+    "url": "/static/js/2.e179491d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a21828de0dd19d8858f9",
+    "url": "/static/js/main.ee227c97.chunk.js"
+  },
+  {
+    "revision": "c2ab9c45931d577cf16e",
+    "url": "/static/js/runtime-main.27b01e2e.js"
+  }
+]);
